75ee1278572b0a40eabe604b2f13e27213fe1ad0
Kevin <c.seas@hotmail.com>
2023-09-13 18:28:00 -0600

    V1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import './assets/css/style.scss'
 import Pokemon from './components/Pokemon'
-// import Card from './components/Card'
-// import Login from './components/Login'
 
 function App() {
   return (
     <main>
       <Pokemon />
-      {/* <Card /> */}
-      {/* <Login /> */}
     </main>
   )
 }
